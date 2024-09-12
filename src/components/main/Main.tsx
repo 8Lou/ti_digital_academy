c99bf4382ec591cbd56986d8b3ee762a83babe19
Lou8 <lou8@mail.com>
2024-09-12 23:26:36 +0300

    Компонент Search

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './main.css';
+import Search from '../searche/Search';
 
 const Main: React.FC = () => {
+  const handleSearch = (query: string) => {
+    console.log('Поиск:', query);
+    // todo: добавить логику обработки запроса
+};
   return (
     <main className="main">
     <div className="block__up"></div>
     <div className="block__down">
-      лодтволмт во
-    </div>
+<h1>Catalog</h1>
+<Search onSearch={handleSearch} />
+</div>
     </main>
   );
 };
