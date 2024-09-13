5c931ea202f887255390fbb7bb8026e9480b3d49
Lou8 <lou8@mail.com>
2024-09-13 12:50:47 +0300

    Bunner & Button styled

--- src/components/baner/Baner.tsx
+++ src/components/baner/Baner.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './baner.css';
 import Button from '../button/Button';
 
-    const Baner: React.FC = () => {
-        return (
-            <div className='baner'>
-                <h1 className='baner__tytle'>Any products from famous brands <br /> with worldwide delivery</h1>
-                <h6 className='baner__text'>We sell smartphones, laptops, clothes, shoes <br /> and many other products at low prices</h6>
-                <Button label="Buy now" onClick={() => {}} />
-            </div>
-        );
-
+const Baner: React.FC = () => {
+    const handleClick = () => {
+        // Здесь вы можете добавить логику для обработки нажатия кнопки
+        console.log('Кнопка нажата!'); // Пример действия
     };
-    export default Baner;
+
+    return (
+        <div className='baner'>
+            <h1 className='baner__title'>Any products from famous brands <br /> with worldwide delivery</h1>
+            <h6 className='baner__text'>We sell smartphones, laptops, clothes, shoes <br /> and many other products at low prices</h6>
+            <Button label="Go to shopping" onClick={handleClick} />
+        </div>
+    );
+};
+export default Baner;
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import './button.css';
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   onClick: () => void;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, disabled, type = 'button', ...rest }) => {
   return (
-    <button onClick={onClick} className="button__plus">
+    <button
+      type={type}
+      className={`button ${disabled ? 'button--disabled' : ''}`}
+      onClick={!disabled ? onClick : undefined}
+      disabled={disabled}
+      {...rest}
+    >
       {label}
     </button>
   );
 };
-
 export default Button;
