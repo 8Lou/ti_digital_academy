5c0ae327f1193f99bbaca5533fd603f2991a723e
Lou8 <lou8@mail.com>
2025-02-06 12:53:10 +0300

    disabling local error checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/home';
 import OneProduct from './pages/oneProduct';
 import Cart from './pages/myCart';
